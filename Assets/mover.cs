44f245069fd2a8d13831b66e60e9b33fe9872fe4
Craig Bauer <craigbauer23@gmail.com>
2022-12-22 16:27:38 -0500

    refactored code and added instructions to the console

--- Assets/mover.cs
+++ Assets/mover.cs
@@ -14,15 +14,21 @@ public class mover : MonoBehaviour
     [SerializeField] float moveSpeed = 15f;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Player Spawned");
-        transform.Translate(0,0,0);
+    void Start() {
+        PrintInstructions();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        MovePlayer();
+    }
+
+    void PrintInstructions() {
+        Debug.Log("Use the arrows to to move player");
+        Debug.Log("Avoid touching the walls");
+    }
+
+    void MovePlayer() {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
