f5cca365ac04721ea50fb55ffbfa7950c17bcf72
Craig Bauer <craigbauer23@gmail.com>
2022-12-23 16:02:05 -0500

    added conditional logic for the hits to only increment score the first time

--- Assets/scripts/objectHit.cs
+++ Assets/scripts/objectHit.cs
@@ -6,8 +6,11 @@ public class objectHit : MonoBehaviour
 {
    private void OnCollisionEnter(Collision other) {
         //Debug.Log("bumped");
+        if(other.gameObject.tag == "Player") {
         GetComponent<MeshRenderer>().material.color = Color.red;
+        gameObject.tag = "Hit";
         //System.Threading.Thread.Sleep(3000); //Pauses the game or stalls it
         //GetComponent<MeshRenderer>().material.color = Color.green;
+        }
    }
 }
--- Assets/scripts/score.cs
+++ Assets/scripts/score.cs
@@ -7,7 +7,9 @@ public class score : MonoBehaviour
     int hits = 0;
 
     private void OnCollisionEnter(Collision other) {
-        Debug.Log("You Bumped Into " + hits + " things");
-        hits++;
+        if(other.gameObject.tag != "Hit") {
+            Debug.Log("You Bumped Into " + hits + " things");
+            hits++;
+        }
     }
 }
